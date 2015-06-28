71bcc6f3c1e5fb09edbed6677a13647dfa2e4e3e
Roman Franchuk <franchukrom@gmail.com>
2015-06-28 21:34:15 +0300

    Skeleton for lights, also fix a bug with object placing

--- Scene.hs
+++ Scene.hs
@@ -3,11 +3,18 @@ module Scene (Scene(Scene), render) where
 import Geometry
 import Codec.Picture
 
+data Light = Light {
+  lightOrigin :: Vector3,
+  lightColor  :: Vector3
+} deriving (Show)
+
 data Scene = Scene {
-  width   :: Int,
-  height  :: Int,
-  fovX    :: Double,
-  spheres :: [Sphere]
+  width        :: Int,
+  height       :: Int,
+  fovX         :: Double,
+  spheres      :: [Sphere],
+  ambientLight :: Vector3,
+  lights       :: [Light]
 } deriving (Show)
 
 renderPlane :: Scene -> Double
@@ -21,7 +28,7 @@ traceRay scene ray =
     processSphere sphere (bestDistance, bestSphere) =
       case ray `intersect` sphere of
         Nothing   -> (bestDistance, bestSphere)
-        Just dist -> (dist, Just sphere)
+        Just dist -> if dist < bestDistance then (dist, Just sphere) else (bestDistance, bestSphere)
   in
     snd (foldr processSphere (infinity, Nothing) (spheres scene))
 
@@ -29,12 +36,12 @@ colorVectorToPixelColor :: Vector3 -> PixelRGB8
 colorVectorToPixelColor (r, g, b) = PixelRGB8 (round (r*255)) (round (g * 255)) (round (b*255))
 
 renderPixel :: Scene -> Int -> Int -> PixelRGB8
-renderPixel (Scene width height fovX spheres) x y =
+renderPixel (Scene width height fovX spheres ambientLight lights) x y =
   case scene `traceRay` ray of
     Nothing     -> PixelRGB8 0 0 0
     Just sphere -> colorVectorToPixelColor (color sphere)
     where
-      scene = Scene width height fovX spheres
+      scene = Scene width height fovX spheres ambientLight lights
       ray = Ray (0, 0, 0) (fromIntegral x - fromIntegral width / 2, fromIntegral height / 2 - fromIntegral y, renderPlane scene)
 
 render :: Scene -> Image PixelRGB8
--- main.hs
+++ main.hs
@@ -3,6 +3,6 @@ import Geometry
 import Scene
 
 main = do
-  let img = render (Scene 1920 1080 (pi/2) [Sphere (0, 0, 4.5) 0.5 (0.5, 0.5, 0.5)])
+  let img = render (Scene 1920 1080 (pi/2) [Sphere (0, 0, 4.5) 0.5 (0.5, 0.5, 0.5), Sphere (0, 0, 3.5) 0.5 (1, 1, 1)] (0, 0, 0) [])
   savePngImage "test.png" (ImageRGB8 img)
   putStrLn "done"
